0a7dbaad155ce73daa1273c2affc7a2df1af001e
Tetr4k <g2012ribeiro@gmail.com>
2022-09-09 10:40:32 -0300

    Create Stack

---
+++ src/classes/Stack/Stack.ts
@@ -0,0 +1,24 @@
+export class Stack<T>{
+	private stack: Array<T>;
+
+	constructor(){
+		this.stack = new Array<T>();
+	}
+
+	public pop(): T | undefined{
+		return this.stack.pop();
+	}
+
+	public push(value: T): boolean{
+		this.stack.push(value);
+		return true;
+	}
+
+	get length(): number{
+		return this.stack.length;
+	}
+
+	public toString(){
+		return this.stack.join(" ");
+	}
+}
